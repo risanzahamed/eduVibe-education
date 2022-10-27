9c63ad1ead4b4ca11188bcf3304ac54ef0b19b23
Risan Ahamed <risanzahamed76@gmail.com>
2022-10-27 17:20:03 +0600

    pdf added

--- src/Pages/CheckOut.js
+++ src/Pages/CheckOut.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Pdf from "react-to-pdf";
+const ref = React.createRef();
 
 
 const CheckOut = () => {
+
     return (
-        <div className=' align-middle text-center justify-center p-40 m-40'>
-            <h1 className='text-4xl text-blue-500 font-bold'>Thanks For Buying This Course</h1>
-            <Link to="/"><button className='btn btn-primary mt-20'>Return To Home</button></Link>
-            
+
+        <div className=' app align-middle text-center justify-center p-40 m-40'>
+            <Pdf targetRef={ref} filename="edu-vibe.pdf">
+                {({ toPdf }) => <button className='btn btn-info text-3xl' onClick={toPdf}>Generate Pdf</button>}
+            </Pdf>
+
+            <div className='text-center' ref={ref}>
+                <h1 className='text-2xl mt-10 text-blue-500 font-bold'>Thanks For Buying This Course</h1>
+            </div>
+                <Link to="/"><button className='btn btn-primary mt-20'>Return To Home</button></Link>
+
+
         </div>
     );
 };
