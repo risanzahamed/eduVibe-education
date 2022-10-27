d469e6ded84d42344fb1132db14d982c8b273bd3
Risan Ahamed <risanzahamed76@gmail.com>
2022-10-27 14:57:44 +0600

    firebase secret setup

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,31 +1,13 @@
-// import { initializeApp } from "firebase/app";
-// const firebaseConfig = {
-//   apiKey: "AIzaSyCStUD5InrZadHFfds72jWMvciYXoKf1sI",
-//   authDomain: "practice-firebase-react-dom.firebaseapp.com",
-//   projectId: "practice-firebase-react-dom",
-//   storageBucket: "practice-firebase-react-dom.appspot.com",
-//   messagingSenderId: "448931082649",
-//   appId: "1:448931082649:web:ebc257a69aa77bacace308"
-// };
-
-
-// const app = initializeApp(firebaseConfig);
-
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAuNTisPSsRXN7GfNNBPz5DiUAtrkmu37o",
-  authDomain: "assignment-10-76a89.firebaseapp.com",
-  projectId: "assignment-10-76a89",
-  storageBucket: "assignment-10-76a89.appspot.com",
-  messagingSenderId: "5273401932",
-  appId: "1:5273401932:web:470fa6ee4139743b1992e9"
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export default app
